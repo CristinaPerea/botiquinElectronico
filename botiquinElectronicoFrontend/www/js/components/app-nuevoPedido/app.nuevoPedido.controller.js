eee18a61d5b07e962d53a0ea3eddd3ed2c0c0401
Cristina Perea <criperrui@gmail.com>
2017-08-27 22:21:19 +0200

    +Añadido botón de navegación al Home cuando se guarda un pedido

--- botiquinElectronicoFrontend/www/js/components/app-nuevoPedido/app.nuevoPedido.controller.js
+++ botiquinElectronicoFrontend/www/js/components/app-nuevoPedido/app.nuevoPedido.controller.js
@@ -1,10 +1,13 @@
 'use strict';
 
-angular.module("app").controller("NuevoPedidoController", ['$http', '$scope', 'ApiService', 'Sesion', '$mdDialog', function($http, $scope, ApiService, Sesion, $mdDialog) {
+angular.module("app").controller("NuevoPedidoController", ['$http', '$scope', 'ApiService', 'Sesion', '$mdDialog', '$state', function($http, $scope, ApiService, Sesion, $mdDialog, $state) {
     this.$onInit = function() {
         $scope.pedido = this.pedido;
     };
 
-
+    $scope.goHome = function () {
+        console.log('Vuelvo al home');
+        $state.go('home');
+    };
 
 }]);
